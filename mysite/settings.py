e4a7fec6daae1851d4c76b6a52c5bc957e4217ef
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-18 17:38:52 +0900

    [fix]refs #3 STATIC_ROOTを修正しパスを追加for Herokuデプロイ

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -130,7 +132,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
 
 DEBUG = False
 
