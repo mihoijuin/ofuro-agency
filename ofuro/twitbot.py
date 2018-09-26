b67f63b514c33f5c3bd1e077ea1258e6fb56c851
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-26 11:11:20 +0900

    [add]refs #43 Botクラスを定義しリプライメソッドを追加
    リプライメソッドではBotのキーを読み込み、引数に指定したユーザネーム宛にリプライを送る
    もしかしたらBotのキー読み込みは別メソッドの方がいいかもしれない

---
+++ ofuro/twitbot.py
@@ -0,0 +1,23 @@
+import tweepy
+
+
+class TweetBot():
+    consumer_key = 'cQJQwfbViw7F4wiKwnayEnNZG'
+    consumer_secret = 'fPgufYNM41qTrXA7nFRgGZLxG8V0nqZewKSbtyyVtnPnBn21gW'
+    access_token = '953117369609568257-X9GsNc0kSIdGaUBaVtLqJyYWYoGNolE'
+    access_token_secret = 'Ssw5hIi7pfkeWKpJVjQrzN3ylsGnVQosiGX3STaRswiIM'
+
+    def reply_result(self, twit_name):
+        # TweepyでBot情報を読み込む
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        api = tweepy.API(auth)
+        # reply用の情報
+        user_name = twit_name
+        result_url = '<result画面のURL>'
+        phrase = '{name}様の代わりにお風呂代行しました♡\n{url}'.format(
+            name=user_name,
+            url=result_url
+            )
+        # reply
+        api.update_status('@' + user_name + '\n' + phrase)
