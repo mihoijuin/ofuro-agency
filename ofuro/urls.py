2e9648f7777cc01f3685a4f042e09273c114db44
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-26 11:07:38 +0900

    [add]refs #45 ボット例外発生時にリダイレクトするページのURLを登録

--- ofuro/urls.py
+++ ofuro/urls.py
@@ -7,4 +7,6 @@ app_name = 'ofuro'
 urlpatterns = [
     path('', login, {'template_name': 'top.html', }, name='top'),
     path('ordered', views.ordered, name='ordered'),    # Twitter認証後リダイレクト
+#    path('result', views.result, name='result'),
+    path('wait', views.wait, name='wait'),    # 例外発生時にリダイレクト
 ]
