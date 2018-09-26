8f32ce1d08f1e19c4022bf66f83fe949f1606abe
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-26 11:10:48 +0900

    [add]refs #51 Twitter Botを呼び出してTwitter認証後リプを返すようにする
    例外発生時はそれ用のテンプレートへ飛ばす（時間ラグは未設定）

--- ofuro/views.py
+++ ofuro/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from social_django.models import UserSocialAuth
 
+from .twitbot import TweetBot
+
 
 @login_required
 def ordered(request):
     user = UserSocialAuth.objects.get(user_id=request.user.id)
+    try:
+        reply_bot = TweetBot()
+        reply_bot.reply_result(user.access_token['screen_name'])
+    except:
+        return render(request, 'wait.html')
     return render(request, 'ordered.html', {'user': user})
