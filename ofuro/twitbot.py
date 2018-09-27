e09d7632d057bff199df8d56d8cebe0e3648e19b
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-27 11:00:51 +0900

    [fix]refs #43 __init__を定義

--- ofuro/twitbot.py
+++ ofuro/twitbot.py
@@ -2,22 +2,23 @@ import tweepy
 
 
 class TweetBot():
-    # TODO __init__で書き換えられないか試す（クラス変数は外部から上書きできてしまってあんまよくないため）
-    consumer_key = 'cQJQwfbViw7F4wiKwnayEnNZG'
-    consumer_secret = 'fPgufYNM41qTrXA7nFRgGZLxG8V0nqZewKSbtyyVtnPnBn21gW'
-    access_token = '953117369609568257-X9GsNc0kSIdGaUBaVtLqJyYWYoGNolE'
-    access_token_secret = 'Ssw5hIi7pfkeWKpJVjQrzN3ylsGnVQosiGX3STaRswiIM'
+    # //d TODO __init__で書き換えられないか試す（クラス変数は外部から上書きできてしまってあんまよくないため）
+    def __init__(self):
+        self.CK = 'cQJQwfbViw7F4wiKwnayEnNZG'
+        self.CS = 'fPgufYNM41qTrXA7nFRgGZLxG8V0nqZewKSbtyyVtnPnBn21gW'
+        self.AT = '953117369609568257-X9GsNc0kSIdGaUBaVtLqJyYWYoGNolE'
+        self.ATS = 'Ssw5hIi7pfkeWKpJVjQrzN3ylsGnVQosiGX3STaRswiIM'
 
     def reply_result(self, twit_name):
         # TweepyでBot情報を読み込む
         # ? Bot情報を読み込むのは別メソッドを定義した方がいいかも？
-        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth.set_access_token(self.access_token, self.access_token_secret)
+        auth = tweepy.OAuthHandler(self.CK, self.CS)
+        auth.set_access_token(self.AT, self.ATS)
         api = tweepy.API(auth)
         # reply用の情報
         user_name = twit_name
         result_url = '<result画面のURL>'
-        phrase = '{name}様の代わりにお風呂代行しました♡\n{url}'.format(
+        phrase = '{name}さまの代わりにお風呂代行しました♡\n{url}'.format(
             name=user_name,
             url=result_url
             )
