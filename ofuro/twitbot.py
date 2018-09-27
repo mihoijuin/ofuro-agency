13685d5e79f2e2197d3fae2fea773e79f1edc9c2
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-27 09:53:00 +0900

    [add]refs #53 コメントメモをBetter Commentsを利用してつける

--- ofuro/twitbot.py
+++ ofuro/twitbot.py
@@ -2,6 +2,7 @@ import tweepy
 
 
 class TweetBot():
+    # TODO __init__で書き換えられないか試す（クラス変数は外部から上書きできてしまってあんまよくないため）
     consumer_key = 'cQJQwfbViw7F4wiKwnayEnNZG'
     consumer_secret = 'fPgufYNM41qTrXA7nFRgGZLxG8V0nqZewKSbtyyVtnPnBn21gW'
     access_token = '953117369609568257-X9GsNc0kSIdGaUBaVtLqJyYWYoGNolE'
@@ -9,6 +10,7 @@ class TweetBot():
 
     def reply_result(self, twit_name):
         # TweepyでBot情報を読み込む
+        # ? Bot情報を読み込むのは別メソッドを定義した方がいいかも？
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.set_access_token(self.access_token, self.access_token_secret)
         api = tweepy.API(auth)
--- ofuro/views.py
+++ ofuro/views.py
@@ -8,6 +8,8 @@ from .twitbot import TweetBot
 @login_required
 def ordered(request):
     user = UserSocialAuth.objects.get(user_id=request.user.id)
+    # TODO テンプレートを読み込む動作を止めずに、Botメソッドを起動するのを10分後にしたい
+    # ? 並行処理かな？とは思っている
     try:
         reply_bot = TweetBot()
         reply_bot.reply_result(user.access_token['screen_name'])
