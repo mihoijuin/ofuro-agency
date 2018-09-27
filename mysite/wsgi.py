79d1ea1a05f6db3ccf01133ad9f5c0468dcba982
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-27 16:28:42 +0900

    [fix]refs #42 Herokuデプロイ用に調整（主にwhitenoise周り)

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,12 +1,13 @@
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 # TODO Djangoかwhitenoiseのバージョンの問題でインポートエラー発生するので後で調べて書き直す
-from whitenoise.django import DjangoWhiteNoise    # Herokuデプロイ用
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
-application = DjangoWhiteNoise(application)   # Herokuデプロイ用
+
