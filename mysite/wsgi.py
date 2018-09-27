d9bafba6fcebc7436a165ece5f6dd09a3f02655a
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-27 17:12:25 +0900

    [fix]refs #42 解決したコメントを削除

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -3,11 +3,8 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-# TODO Djangoかwhitenoiseのバージョンの問題でインポートエラー発生するので後で調べて書き直す
-
+# // TODO Djangoかwhitenoiseのバージョンの問題でインポートエラー発生するので後で調べて書き直す
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = Cling(get_wsgi_application())
-
-
