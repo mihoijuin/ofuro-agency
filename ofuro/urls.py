ebcc1b19eb398142ca0f2dfcff5e2a6030635d32
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-28 13:53:10 +0900

    [add]refs #61 ログアウト処理用のURLを追加

--- ofuro/urls.py
+++ ofuro/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from . import views
 
 app_name = 'ofuro'
 
 urlpatterns = [
     path('', login, {'template_name': 'top.html', }, name='top'),
+    # めんどいのでログアウト後の画面はトップにしちゃった
+    # ログアウトしたらアラートで「ログアウトしました」って出たら理想
+    path('logout', logout, {'template_name': 'top.html', }, name='top'),
     path('ordered', views.ordered, name='ordered'),    # Twitter認証後リダイレクト
     path('result1', views.result1, name='result1'),
     path('result2', views.result2, name='result2'),
