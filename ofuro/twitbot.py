441f9462ccb00eabbb4803a8695461da5bc006d1
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-28 14:35:41 +0900

    [fix]refs #6 画像差し替えに伴いURL出現確率を修正

--- ofuro/twitbot.py
+++ ofuro/twitbot.py
@@ -31,7 +31,7 @@ class TweetBot():
         result_url1 = ['https://ofuro-agency.herokuapp.com/result1']    # ネタ枠
         result_url2 = ['https://ofuro-agency.herokuapp.com/result2']    # ゲスト枠
         result_url3 = ['https://ofuro-agency.herokuapp.com/result3']    # 犬枠
-        result_urls = result_url1 * 20 + result_url2 * 5 + result_url3 * 2
+        result_urls = result_url1 * 10 + result_url2 * 2 + result_url3 * 5
         # 連投エラーの保険のためにフレーズを複数用意しランダムに選ぶ
         phrases = [
             'お風呂代わりに入ってきました♡',
