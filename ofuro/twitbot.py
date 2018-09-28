c6778e29f3b3536f629d5c3df0bdf27308d3af23
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-09-28 09:39:23 +0900

    [add]refs #35 #61 Botのリプライ内容を本番向けに変更
    - 連投エラー避けるためにテキストをランダムに選ぶ
    - result画面のURLをネタ枠が多くなるようにランダムに選ぶ

--- ofuro/twitbot.py
+++ ofuro/twitbot.py
@@ -1,4 +1,5 @@
 import tweepy
+import random
 
 
 class TweetBot():
@@ -9,6 +10,8 @@ class TweetBot():
         self.AT = '953117369609568257-X9GsNc0kSIdGaUBaVtLqJyYWYoGNolE'
         self.ATS = 'Ssw5hIi7pfkeWKpJVjQrzN3ylsGnVQosiGX3STaRswiIM'
 
+
+    # // TODO phraseをランダムに変えるようにする（Twitterのエラー対処のため）
     def reply_result(self, twit_name):
         # TweepyでBot情報を読み込む
         # ? Bot情報を読み込むのは別メソッドを定義した方がいいかも？
@@ -16,11 +19,22 @@ class TweetBot():
         auth.set_access_token(self.AT, self.ATS)
         api = tweepy.API(auth)
         # reply用の情報
+        # // TODO phraseの内容は連投エラーの保険のためにランダムにした方が良さそう
         user_name = twit_name
-        result_url = '<result画面のURL>'
-        phrase = '{name}さまの代わりにお風呂代行しました♡\n{url}'.format(
-            name=user_name,
-            url=result_url
-            )
+        # resultページのURLをネタ枠が多くなるようにランダムに選ぶ
+        result_url1 = ['https://ofuro-agency.herokuapp.com/result1']    # ネタ枠
+        result_url2 = ['https://ofuro-agency.herokuapp.com/result2']    # ゲスト枠
+        result_url3 = ['https://ofuro-agency.herokuapp.com/result3']    # 犬枠
+        result_urls = result_url1 * 20 + result_url2 * 5 + result_url3 * 2
+        phrases = [
+            'お風呂代わりに入ってきました♡',
+            'お待たせいたしました♡お風呂代わりに入ってきました♡',
+            'おふろ代わりに入ってきました♡',
+            'おまたせいたしました♡おふろ代わりに入ってきました♡'
+            ]
+        phrase = '{text}\n{url}'.format(
+            text=random.choice(phrases),
+            url=random.choice(result_urls)
+        )
         # reply
-        api.update_status('@' + user_name + '\n' + phrase)
+        api.update_status('@' + user_name + '\n\n' + phrase)
