4f03e27ddf7db02c8c839ff21eece701427b358e
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-10-03 10:05:46 +0900

    [update]refs #99 url名をわかりやすい名前に変更（result-dogなど

--- ofuro/urls.py
+++ ofuro/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     # ログアウトしたらアラートで「ログアウトしました」って出たら理想
     path('logout', logout, {'template_name': 'top.html', }, name='top'),
     path('ordered', views.ordered, name='ordered'),    # Twitter認証後リダイレクト
-    path('result1', views.result1, name='result1'),
-    path('result2', views.result2, name='result2'),
-    path('result3', views.result3, name='result3'),
-    path('result4', views.result4, name='result4'),
-    path('result5', views.result5, name='result5'),
-    path('result6', views.result6, name='result6'),
+    path('result-monkey', views.result-monkey, name='result-monkey'),
+    path('result-dog', views.result-dog, name='result-dog'),
+    path('result-duck', views.result-duck, name='result-duck'),
+    path('result-nananana', views.result-nananana, name='result-nananana'),
+    path('result-money', views.result-money, name='result-money'),
+    path('result-oyaji', views.result-oyaji, name='result-oyaji'),
     path('wait', views.wait, name='wait'),    # 例外発生時にリダイレクト
 ]
