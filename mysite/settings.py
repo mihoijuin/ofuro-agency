838d2cffacd1c6cbea41e14f86ba32ca863fc3d7
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-10-03 14:00:17 +0900

    [fix]heroku 500エラー解消実験static_rootを変えてみる

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,8 +116,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # Herokuデプロイ用
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
