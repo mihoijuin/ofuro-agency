1029cf9da3c1b9780a7c990f3d8f7d8201b8daa4
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-10-10 13:06:26 +0900

    [update]refs #125 毎回トップページに飛ぶのめんどくさかったのでもう一度お風呂入るボタンを押したらランダム結果に飛ぶようにした

--- ofuro/views.py
+++ ofuro/views.py
@@ -10,29 +10,30 @@ from .crypturl import AESCipher
 
 
 def encrypt_path(path):    # urls.pyでも使い回せるようにURL全部でなくパスを暗号化
+    '''結果ページのURLをパッと見てわからなくするために暗号化'''
     key = 'poriporiporiofuroporipori'
     aes = AESCipher(key)
     enc_path = aes.encrypt(aes.key, path)
     return enc_path
 
 
+# ランダム表示する結果URLリストを作成（URLは暗号化）
+# 複数のビューで呼び出すため、グローバル変数に代入
+root_url = 'https://ofuro-agency.herokuapp.com/'
+result_monkey = [root_url + 'LofG1lC3uIf7XnD6awGDw==']
+result_dog = [root_url + encrypt_path('result-dog')]
+result_duck = [root_url + encrypt_path('result-duck')]
+result_nananana = [root_url + encrypt_path('result-nananana')]
+result_money = [root_url + encrypt_path('result-money')]
+result_oyaji = [root_url + encrypt_path('result-oyaji')]
+result_seabiscuit = [root_url + 'mmDtzcWuWnC0pYkMBEugKgR5rXLRQRKv02WLfTIvzM=']
+result_momu = [root_url + encrypt_path('result-momu')]
+# resultページのURLをネタ枠が多くなるようにランダムに選ぶ
+result_urls = result_monkey * 2 + result_dog * 1 + result_duck * 1 + result_nananana * 1 + result_money * 3 + result_oyaji * 3 + result_seabiscuit * 1 + result_momu * 1
+
+
 def top(request):
-    # URLを暗号化
-    try:
-        root_url = 'https://ofuro-agency.herokuapp.com/'
-        result_monkey = [root_url + 'LofG1lC3uIf7XnD6awGDw==']
-        result_dog = [root_url + encrypt_path('result-dog')]
-        result_duck = [root_url + encrypt_path('result-duck')]
-        result_nananana = [root_url + encrypt_path('result-nananana')]
-        result_money = [root_url + encrypt_path('result-money')]
-        result_oyaji = [root_url + encrypt_path('result-oyaji')]
-        result_seabiscuit = [root_url + 'mmDtzcWuWnC0pYkMBEugKgR5rXLRQRKv02WLfTIvzM=']
-        result_momu = [root_url + encrypt_path('result-momu')]
-        # resultページのURLをネタ枠が多くなるようにランダムに選ぶ
-        result_urls = result_monkey * 2 + result_dog * 1 + result_duck * 1 + result_nananana * 1 + result_money * 5 + result_oyaji * 5 + result_seabiscuit * 1 + result_momu * 1
-        return render(request, 'top.html', {'result_urls': result_urls})
-    except:
-        return redirect('/wait')
+    return render(request, 'top.html', {'result_urls': result_urls})
 
 
 def wait(request):
@@ -44,43 +45,46 @@ def wait(request):
 # resultへすんなり遷移させたくなかったのでsleep挟む
 def result_monkey(request):
     time.sleep(2)
-    return render(request, 'result_monkey.html')
+    return render(request, 'result_monkey.html', {'result_urls': result_urls})
 
 
 def result_dog(request):
     time.sleep(2)
-    return render(request, 'result_dog.html')
+    return render(request, 'result_dog.html', {'result_urls': result_urls})
 
 
 def result_duck(request):
     time.sleep(2)
-    return render(request, 'result_duck.html')
+    return render(request, 'result_duck.html', {'result_urls': result_urls})
 
 
 def result_nananana(request):
     time.sleep(2)
-    return render(request, 'result_nananana.html')
+    return render(
+        request, 'result_nananana.html', {'result_urls': result_urls}
+        )
 
 
 def result_money(request):
     time.sleep(2)
-    return render(request, 'result_money.html')
+    return render(request, 'result_money.html', {'result_urls': result_urls})
 
 
 def result_oyaji(request):
     time.sleep(2)
-    return render(request, 'result_oyaji.html')
+    return render(request, 'result_oyaji.html', {'result_urls': result_urls})
 
 
 def result_seabiscuit(request):
     time.sleep(2)
-    return render(request, 'result_seabiscuit.html')
-
+    return render(
+        request, 'result_seabiscuit.html', {'result_urls': result_urls}
+        )
 
 
 def result_momu(request):
     time.sleep(2)
-    return render(request, 'result_momu.html')
+    return render(request, 'result_momu.html', {'result_urls': result_urls})
 
 # @login_required
 # def ordered(request):
