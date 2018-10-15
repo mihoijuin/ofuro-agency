230e1d6d3635142f7d4b00f94b6cb38958a17ab9
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-10-15 19:33:26 +0900

    [update]refs #94 可愛い子ですぎてアレなので動物の確率アップ

--- ofuro/views.py
+++ ofuro/views.py
@@ -37,10 +37,11 @@ result_sorami = [root_url + encrypt_path('result-sorami')]
 result_amanatu = [root_url + 'Hplos4rCjLihwEng3Ow==']
 result_mam = [root_url + 'gunY0ZDCpndh1PkTaRRNw==']
 # resultページのURLをネタ枠が多くなるようにランダムに選ぶ
-result_urls = result_monkey + result_dog + result_duck + result_money * 8\
-    + result_oyaji * 8 + result_nananana + result_seabiscuit + result_momu\
-    + result_mam + result_sana + result_chihiro + result_muscle_nananana * 3\
-    + result_higuma + result_amanatu + result_yukariko + result_sorami
+result_urls = result_monkey * 3 + result_dog * 3 + result_duck * 3\
+    + result_money * 6 + result_oyaji * 8 + result_nananana\
+    + result_seabiscuit + result_momu + result_mam + result_sana\
+    + result_chihiro + result_muscle_nananana * 3 + result_higuma\
+    + result_amanatu + result_yukariko + result_sorami
 
 
 def top(request):
