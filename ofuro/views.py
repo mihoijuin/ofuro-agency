ff776a3ba20302b3f21ff3b03198fae2c837f259
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-11-11 08:25:08 +0900

    [add]refs #134 結果ビューを作成（URLは暗号化されてない状態）

--- ofuro/views.py
+++ ofuro/views.py
@@ -2,18 +2,16 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
-from social_django.models import UserSocialAuth
+from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
 
-from .twitbot import TweetBot
+from .models import OfuroResult, GuestIntroduce
 from .crypturl import AESCipher
 
-
-def encrypt_path(path):    # urls.pyでも使い回せるようにURL全部でなくパスを暗号化
-    '''結果ページのURLをパッと見てわからなくするために暗号化'''
-    aes = AESCipher()
-    enc_path = aes.encrypt(path)
-    return enc_path
+# def encrypt_path(path):    # urls.pyでも使い回せるようにURL全部でなくパスを暗号化
+#     '''結果ページのURLをパッと見てわからなくするために暗号化'''
+#     aes = AESCipher()
+#     enc_path = aes.encrypt(path)
+#     return enc_path
 
 
 def page_transition(request):
@@ -63,165 +61,19 @@ def wait(request):
     return render(request, 'wait.html')
 
 
-# TODO できたら各ビューで画像数種類をランダムに表示できるようにしたい...
-# TODO 画像を管理画面から投稿してどんどんページを作成できるようにするのが理想
-# resultへすんなり遷移させたくなかったのでsleep挟む
-def result_monkey(request):
-    try:
-        return render(request, 'result_monkey.html')
-    except:
-        return redirect('/wait')
-
-
-def result_dog(request):
-    try:
-        return render(request, 'result_dog.html')
-    except:
-        return redirect('/wait')
-
-
-def result_duck(request):
-    try:
-        return render(request, 'result_duck.html')
-    except:
-        return redirect('/wait')
-
-
-def result_nananana(request):
-    try:
-        return render(request, 'result_nananana.html')
-    except:
-        return redirect('/wait')
-
-
-def result_muscle_nananana(request):
-    try:
-        return render(request, 'result_muscle_nananana.html')
-    except:
-        return redirect('/wait')
-
-
-def result_money(request):
-    try:
-        return render(request, 'result_money.html')
-    except:
-        return redirect('/wait')
-
-
-def result_oyaji(request):
-    try:
-        return render(request, 'result_oyaji.html')
-    except:
-        return redirect('/wait')
-
-
-def result_seabiscuit(request):
-    try:
-        return render(request, 'result_seabiscuit.html')
-    except:
-        return redirect('/wait')
-
-
-def result_momu(request):
-    try:
-        return render(request, 'result_momu.html')
-    except:
-        return redirect('/wait')
-
-
-def result_mam(request):
-    try:
-        return render(request, 'result_mam.html')
-    except:
-        return redirect('/wait')
-
-
-def result_sana(request):
-    try:
-        return render(request, 'result_sana.html')
-    except:
-        return redirect('/wait')
-
-
-def result_chihiro(request):
-    try:
-        return render(request, 'result_chihiro.html')
-    except:
-        return redirect('/wait')
-
-
-def result_higuma(request):
-    try:
-        return render(request, 'result_higuma.html')
-    except:
-        return redirect('/wait')
-
-
-def result_amanatu(request):
-    try:
-        return render(request, 'result_amanatu.html')
-    except:
-        return redirect('/wait')
-
-
-def result_sorami(request):
-    try:
-        return render(request, 'result_sorami.html')
-    except:
-        return redirect('/wait')
-
-
-def result_yukariko(request):
-    try:
-        return render(request, 'result_yukariko.html')
-    except:
-        return redirect('/wait')
-
-
-def result_beryl(request):
-    try:
-        return render(request, 'result_beryl.html')
-    except:
-        return redirect('/wait')
-
-
-def result_haijoi(request):
-    try:
-        return render(request, 'result_haijoi.html')
-    except:
-        return redirect('/wait')
-
-
-def result_mokyu(request):
-    try:
-        return render(request, 'result_mokyu.html')
-    except:
-        return redirect('/wait')
-
-
-def result_mareru(request):
-    try:
-        return render(request, 'result_mareru.html')
-    except:
-        return redirect('/wait')
-
-
-def result_ain(request):
-    try:
-        return render(request, 'result_ain.html')
-    except:
-        return redirect('/wait')
-
-
-def result_momiji(request):
-    try:
-        return render(request, 'result_momiji.html')
-    except:
-        return redirect('/wait')
-
-
-def result_imari(request):
-    try:
-        return render(request, 'result_imari.html')
-    except:
-        return redirect('/wait')
+def result_detail(request, pk):
+    result = get_object_or_404(OfuroResult, pk=pk)
+    guest_introduces = get_list_or_404(GuestIntroduce, result=pk)
+    # aes = AESCipher()
+    # result_id = aes.decrypt(encrypt_pk)
+    # result = get_object_or_404(OfuroResult, pk=pk)
+    # guest_introduces = get_list_or_404(GuestIntroduce, result=result_id)
+    return render(
+        request,
+        'result_detail.html',
+        {
+            'result': result,
+            'guest_introduces': guest_introduces,
+            'path': pk
+        }
+    )
