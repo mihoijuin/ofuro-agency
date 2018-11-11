bda75438a86b18bcd67da6c9e087d6348be4ab96
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-11-11 19:28:27 +0900

    [add]refs #143 DB移行後のURL暗号化を実装

--- ofuro/urls.py
+++ ofuro/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import login, logout
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 from .crypturl import AESCipher
@@ -10,8 +10,8 @@ aes = AESCipher()
 # 結局うけつけるのはpathで指定した内容なので複合化意味なくね？ってなった
 urlpatterns = [
     path('', views.top, name='top'),
-    path('wait', views.wait, name='wait'),    # 例外発生時にリダイレクト
     path('page-transition', views.page_transition, name='page_transition'),
-    path('results/<str:pk>', views.result_detail, name='result_detail'),
-    path('{}'.format(aes.encrypt('ain')), views.result_detail),
+    # re_path(r'^results/(?P<enc_pk>[_!(%=0-9a-zA-Z])/$', views.page_transition, name='page_transition'),
+    path('results/<str:enc>', views.result_detail, name='result_detail'),
+    path('{}'.format(aes.encrypt('dog')), views.result_detail),
 ]
--- ofuro/views.py
+++ ofuro/views.py
@@ -2,17 +2,42 @@ from django.shortcuts import (get_list_or_404, get_object_or_404, redirect,
                               render)
 
 from .models import GuestIntroduce, OfuroResult
+from .crypturl import AESCipher
 
 
 def page_transition(request):
+    aes = AESCipher()
+    monkey = [aes.encrypt('monkey')]
+    dog = [aes.encrypt('dog')]
+    duck = [aes.encrypt('duck')]
+    money = [aes.encrypt('money')]
+    oyaji = [aes.encrypt('oyaji')]
+    nananana = [aes.encrypt('nananana')]
+    seabiscuit = [aes.encrypt('seabiscuit')]
+    momu = [aes.encrypt('momu')]
+    mam = [aes.encrypt('mam')]
+    sana = [aes.encrypt('sana')]
+    chihiro = [aes.encrypt('chihiro')]
+    kintanikuo = [aes.encrypt('kintanikuo')]
+    higuma = [aes.encrypt('higuma')]
+    amanatu = [aes.encrypt('amanatu')]
+    yukariko = [aes.encrypt('yukariko')]
+    sorami = [aes.encrypt('sorami')]
+    beryl = [aes.encrypt('beryl')]
+    haijoi = [aes.encrypt('haijoi')]
+    mokyu = [aes.encrypt('mokyu')]
+    mareru = [aes.encrypt('mareru')]
+    ain = [aes.encrypt('ain')]
+    momiji = [aes.encrypt('momiji')]
+    imari = [aes.encrypt('imari')]
     # resultページのURLをネタ枠が多くなるようにランダムに選ぶ
-    result_paths = ['monkey'] * 5 + ['dog'] * 5 + ['duck']\
-        + ['money'] * 5 + ['oyaji'] * 5 + ['nananana']\
-        + ['seabiscuit'] + ['momu'] * 3 + ['mam'] + ['sana']\
-        + ['chihiro'] + ['kintanikuo'] * 3 + ['higuma']\
-        + ['amanatu'] + ['yukariko'] + ['sorami'] + ['beryl']\
-        + ['haijoi'] + ['mokyu'] + ['mareru'] + ['ain']\
-        + ['momiji'] + ['imari']
+    result_paths = monkey * 5 + dog * 5 + duck\
+        + money * 5 + oyaji * 5 + nananana\
+        + seabiscuit + momu * 3 + mam + sana\
+        + chihiro + kintanikuo * 3 + higuma\
+        + amanatu + yukariko + sorami + beryl\
+        + haijoi + mokyu + mareru + ain\
+        + momiji + imari
     return render(
         request, 'page_transition.html',
         {'result_paths': result_paths}
@@ -23,8 +48,10 @@ def top(request):
     return render(request, 'top.html')
 
 
-def result_detail(request, pk):
-    result = get_object_or_404(OfuroResult, pk=pk)
+def result_detail(request, enc):
+    aes = AESCipher()
+    pk = aes.decrypt(enc)
+    result = get_object_or_404(OfuroResult, result_id=pk)
     # ゲストの場合はゲスト紹介文をテンプレートに渡す
     try:
         guest_introduces = get_list_or_404(GuestIntroduce, result=pk)
@@ -34,7 +61,7 @@ def result_detail(request, pk):
             {
                 'result': result,
                 'guest_introduces': guest_introduces,
-                'path': pk
+                'path': enc,
             }
         )
     except:
