ad97c2001ff68165ba29e98716a593ded90cb9b5
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-11-11 10:47:32 +0900

    [update]refs #128 ランダムにDB導入後のURLが選ばれるように

--- ofuro/views.py
+++ ofuro/views.py
@@ -15,38 +15,14 @@ from .crypturl import AESCipher
 
 
 def page_transition(request):
-    # ランダム表示する結果URLリストを作成（URLは暗号化）
-    result_monkey = [encrypt_path('result-monkey')]
-    result_dog = [encrypt_path('result-dog')]
-    result_duck = [encrypt_path('result-duck')]
-    result_nananana = [encrypt_path('result-nananana')]
-    result_muscle_nananana = [encrypt_path('result-muscle_nananana')]
-    result_money = [encrypt_path('result-money')]
-    result_oyaji = [encrypt_path('result-oyaji')]
-    result_seabiscuit = [encrypt_path('result-seabiscuit')]
-    result_momu = [encrypt_path('result-momu')]
-    result_sana = [encrypt_path('result-sana')]
-    result_chihiro = [encrypt_path('result-chihiro')]
-    result_higuma = [encrypt_path('result-higuma')]
-    result_yukariko = [encrypt_path('result-yukariko')]
-    result_sorami = [encrypt_path('result-sorami')]
-    result_mokyu = [encrypt_path('result-mokyu')]
-    result_momiji = [encrypt_path('result-momiji')]
-    result_imari = [encrypt_path('result-imari')]
-    result_ain = [encrypt_path('result-ain')]
-    result_mareru = [encrypt_path('result-mareru')]
-    result_haijoi = [encrypt_path('result-haijoi')]
-    result_amanatu = [encrypt_path('result-amanatu')]
-    result_mam = [encrypt_path('result-mam')]
-    result_beryl = [encrypt_path('result-beryl')]
     # resultページのURLをネタ枠が多くなるようにランダムに選ぶ
-    result_paths = result_monkey * 5 + result_dog * 5 + result_duck\
-        + result_money * 5 + result_oyaji * 5 + result_nananana\
-        + result_seabiscuit + result_momu * 3 + result_mam + result_sana\
-        + result_chihiro + result_muscle_nananana * 3 + result_higuma\
-        + result_amanatu + result_yukariko + result_sorami + result_beryl\
-        + result_haijoi + result_mokyu + result_mareru + result_ain\
-        + result_momiji + result_imari
+    result_paths = ['monkey'] * 5 + ['dog'] * 5 + ['duck']\
+        + ['money'] * 5 + ['oyaji'] * 5 + ['nananana']\
+        + ['seabiscuit'] + ['momu'] * 3 + ['mam'] + ['sana']\
+        + ['chihiro'] + ['kintanikuo'] * 3 + ['higuma']\
+        + ['amanatu'] + ['yukariko'] + ['sorami'] + ['beryl']\
+        + ['haijoi'] + ['mokyu'] + ['mareru'] + ['ain']\
+        + ['momiji'] + ['imari']
     return render(
         request, 'page_transition.html',
         {'result_paths': result_paths}
@@ -84,7 +60,3 @@ def result_detail(request, pk):
                 'path': pk
             }
         )
-    # aes = AESCipher()
-    # result_id = aes.decrypt(encrypt_pk)
-    # result = get_object_or_404(OfuroResult, pk=pk)
-    # guest_introduces = get_list_or_404(GuestIntroduce, result=result_id)
