a6afef5253ab6f4dc15e04a84f0f9b469e7d55c6
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-11-14 10:54:43 +0900

    [add]refs #141 メディアファイル公開用のURLを追加

--- ofuro/urls.py
+++ ofuro/urls.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import login, logout
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 from .crypturl import AESCipher
@@ -12,5 +14,11 @@ urlpatterns = [
     path('', views.top, name='top'),
     path('page-transition', views.page_transition, name='page_transition'),
     path('results/<str:enc>', views.result_detail, name='result_detail'),
+    path(aes.encrypt('ain'), views.result_detail),
     path('wait', views.wait, name='wait'),
 ]
+
+# メディアファイル公開用のURL
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
