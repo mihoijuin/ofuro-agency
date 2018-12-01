79a01b67583b1da2d82eabf420c4c67cd0af52b8
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-12-01 17:08:23 +0900

    [remove]refs #155 使わなくなったJSコードを削除

--- ofuro/static/js/specialOrder.js
+++ ofuro/static/js/specialOrder.js
@@ -12,20 +12,8 @@ if(localStorage.getItem) {
 }
 
 /* 指名代行ボタンを押したら20point消費 */
-// 複数あるボタン全てに処理をするのでクラス名から要素を取得
 function resetPoint() {
     var point = Number(localStorage.getItem('ofuroPoint'));
     point -= 20;
     localStorage.setItem('ofuroPoint', point);
 }
-
-// var orderBtn = document.getElementsByClassName('orderBtn');
-// for(i = 0; i <= orderBtn.length; i++) {
-//     // 全てのボタンにstyleを適用させるため、繰り返し処理
-//     orderBtn[i].addEventListener('click', function() {
-//         console.log('push')
-//         var point = Number(localStorage.getItem('ofuroPoint'));
-//         point -= 20;
-//         localStorage.setItem('ofuroPoint', point);
-//     });
-// }
