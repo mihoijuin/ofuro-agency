40a363f7151c6288afce3bcc71da393d1f6f3b7d
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-12-08 11:31:20 +0900

    [add]refs #155 お風呂マークをポイントのまわりに表示させる

--- ofuro/static/js/showPoint.js
+++ ofuro/static/js/showPoint.js
@@ -2,8 +2,8 @@ var pointNav =  document.getElementById('pointNav');
 
 if(localStorage.getItem('ofuroPoint')) {
     var point = localStorage.getItem('ofuroPoint');
-    pointNav.innerText = point + ' ポイント';
+    pointNav.innerText = '♨️ ' + point + ' ポイント♨️';
 } else {
     var point = 0;
-    pointNav.innerText =  point + ' ポイント';
+    pointNav.innerText = '♨️ ' + point + ' ポイント♨️';
 }
